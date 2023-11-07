6d23f603eed1abd97710eb7bdd23894ec261e438
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-11-07 08:31:06 -0400

    Update locadora.py

--- Prova_02/locadora.py
+++ Prova_02/locadora.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+
 def exibir_tabela_carros(carros):
     os.system("cls")
     print("================================ LOCADORA DE VEICULOS ================================")
@@ -9,14 +10,17 @@ def exibir_tabela_carros(carros):
         for carro in carros_categoria:
             print("{:<10} {:<10} {:<15} {:<10} {:<10} R${:<14.2f} {:<10}".format(categoria, carro["marca"], carro["modelo"], carro["ano"], carro["cor"], carro["preco_por_dia"],carro["disponivel"]))
 
-def carregar_carros():
+
+
+def carregar_arquivo_carros():
     with open('carros.json','r') as f:
         texto = f.read()
         abrir_carros = json.loads(texto)
     return abrir_carros
 
+
 def menu():
-    carros= carregar_carros()
+    carros= carregar_arquivo_carros()
     while True:
         print(exibir_tabela_carros(carros)) 
         op = input('\n(1) Locação de Veículos\n(2) Visualizar lista de clientes\n:')
@@ -29,8 +33,15 @@ def menu():
             
 def alugar_carro(carros):
     op = input("(1) Alugar veículo\n(2) Devolver um veículo alugado\n")
+    if op == '1':
+        #Cadastrar usuario a alugar carro
+    elif op == '2':
+        #devolver carro alugado pelo cliente
+
 
 def lista_de_clientes(carros):
+    #imprimir lista de clientes
+
 
 if __name__=='__main__':
     menu()
