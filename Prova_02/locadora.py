3eda166de0f1b4e964abacd6c989f7a5d7b459a0
Tayane Emanuely da Silva <146960458+SilvaTayane@users.noreply.github.com>
2023-11-07 09:53:32 -0400

    Update locadora.py

--- Prova_02/locadora.py
+++ Prova_02/locadora.py
@@ -17,33 +17,51 @@ def carregar_arquivo_carros():
         abrir_carros = json.loads(texto)
     return abrir_carros
 
+def reservar_carro(carros):
+    #Parte do João Vitor#
+    # remover 1 da quantidade de veiculos disponivel
+    tipo_carro = input('Qual modelo de carro:')
+    dias = int(input('Quantidade de dias de aluguel:'))
+    op_2 = input('\n(1) Confirmar contrato\n(2) Mudar o veiculo\n(3) Encerrar\n')
+    if tipo_carro in carros.keys():
+        
+    if(op_2=='1'):
+            alugar_carro(carros,  tipo_carro, dias)
+    elif(op_2=='2'):
+        reservar_carro(carros)
+    elif(op_2=='3'):
+        return None
 
+    
 
-def alugar_carro(carros):
-    op = input("(1) Alugar veículo\n(2) Devolver um veículo alugado\n")
-    if op == '1':
-        #Cadastrar usuario a alugar carro
-    elif op == '2':
-        #devolver carro alugado pelo cliente
+def alugar_carro(carros, tipo_carro, dias):
+    nome = input('Nome:')
+    idade = int(input('Idade:'))
+    if idade < 25:
+        conta = dias * carros[tipo_carro][4] + 30
+    else:
+        conta = dias * carros[tipo_carro][4]
+    cpf = int(input('CPF:'))
+    while cpf < 11 :
+        cpf = int(input('CPF:'))  
+    
+    print('======CONTRATO DE LOCAÇÃO======\n')
+    print('{} portador do CPF {} está alugando o veiculo {} \nno periodo de {} dias , que ficará no valor de R${}'.format(nome, cpf, tipo_carro, dias, conta))
 
-
-
-#imprimir lista de clientes
-def lista_de_clientes(carros):
 
 
 
 #Menu
 def menu():
-    carros= carregar_arquivo_carros()
+    carros= carregar_carros()
     while True:
         print(exibir_tabela_carros(carros)) 
-        op = input('\n(1) Locação de Veículos\n(2) Visualizar lista de clientes\n:')
+        op = input('\n(1) Reservar veiculo\n(2) Devolver veiculo\n:')
         
         if(op=='1'):
-            alugar_carro(carros)
+            reservar_carro(carros)
         elif(op=='2'):
-            lista_de_clientes(carros)
+            devolver_carro(carros)
 
         
 
